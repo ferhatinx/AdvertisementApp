5fc4e916e8a84a09367ce8e3ec6192c1a9650f1e
ferhatinx <ferhatinx@gmail.com>
2023-01-02 00:38:00 +0300

    sendform1

---
+++ UI/Controllers/AdvertisementController.cs
@@ -0,0 +1,39 @@
+﻿using Business.Interfaces;
+using Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class AdvertisementController : Controller
+    {
+        private readonly IAppUserService _appuserService;
+
+        public AdvertisementController(IAppUserService appuserService)
+        {
+            _appuserService = appuserService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Member")]
+        public async Task<IActionResult> Send(int advertisementid) 
+        {
+            var userid = int.Parse((User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)).Value);
+            var userResponse =await  _appuserService.GetByIdAsync<AppUserListDto>(userid);
+
+            ViewBag.GenderId = userResponse.Data.GenderId;
+            return View(new AdvertisementAppUserCreateModel 
+            { 
+                AdvertisementId = advertisementid,
+                AppUserId = userid
+            });
+        }
+    }
+}
---
+++ UI/Models/AdvertisementAppUserCreateModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.VisualBasic;
+using System;
+
+namespace UI.Models
+{
+    public class AdvertisementAppUserCreateModel
+    {
+        public int AdvertisementId { get; set; }
+
+        public int AppUserId { get; set; }
+
+        public int AdvertisementAppUserStatusId { get; set; }
+
+        public int MilitaryStatusId { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int WorkExperience { get; set; }
+
+        public IFormFile CvFile { get; set; }
+
+    }
+}
